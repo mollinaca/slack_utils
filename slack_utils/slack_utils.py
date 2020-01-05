f547d1ff3a03cc47ae045dc6df4e9cfed2d85cdf
mollinaca <github@mail.watarinohibi.tokyo>
2020-01-05 18:02:32 +0900

    update ファイル構造を調整、構造はこれでfix

--- slack_utils/slack_utils.py
+++ slack_utils/slack_utils.py
@@ -1,7 +1,8 @@
 import sys
 import logging
 from optparse import OptionParser
-from slack_utils.functions import api_test, auth_test, incoming_webhook, post, post_quote, conv_list
+#from slack_utils.functions import api_test, auth_test, incoming_webhook, post, post_quote, conv_list
+from slack_utils.functions import api_test
 """
 実行例:
   $ slack_utils api_test
@@ -30,43 +31,43 @@ def command (argv:list):
     elif command == "api_test":
         api_test ()
 
-    elif command == "auth_test":
-        auth_test ()
-
-    elif command == "incoming_webhook":
-        """
-        incoming-webhook を使ったシンプルなメッセージのPOST
-        """
-        if argv[1:2]:
-            incoming_webhook (argv[1:])
-        else:    
-            logging.warning('Usage -> $ slack_utils incoming_webhook ${message}')
-            exit (1)
-
-    elif command == "post":
-        if argv[1:2]:
-            post (argv[1:])
-        else:    
-            logging.warning('Usage -> $ slack_utils post ${message}')
-            exit (1)
-
-    elif command == "post_quote":
-        if argv[1:2]:
-            post_quote (argv[1:])
-        else:    
-            logging.warning('Usage -> $ slack_utils post_quote ${message}')
-            exit (1)
-
-
-    elif command == "conv_list":
-        """
-        $ slack_utils conv_list [public|private] [true|false]'
-        default:
-            arg2(type): both of public and private 
-            arg3(exclude_archive): true
-        """
-        conv_list (argv[1:])
-
+#    elif command == "auth_test":
+#        auth_test ()
+#
+#    elif command == "incoming_webhook":
+#        """
+#        incoming-webhook を使ったシンプルなメッセージのPOST
+#        """
+#        if argv[1:2]:
+#            incoming_webhook (argv[1:])
+#        else:    
+#            logging.warning('Usage -> $ slack_utils incoming_webhook ${message}')
+#            exit (1)
+#
+#    elif command == "post":
+#        if argv[1:2]:
+#            post (argv[1:])
+#        else:    
+#            logging.warning('Usage -> $ slack_utils post ${message}')
+#            exit (1)
+#
+#    elif command == "post_quote":
+#        if argv[1:2]:
+#            post_quote (argv[1:])
+#        else:    
+#            logging.warning('Usage -> $ slack_utils post_quote ${message}')
+#            exit (1)
+#
+#
+#    elif command == "conv_list":
+#        """
+#        $ slack_utils conv_list [public|private] [true|false]'
+#        default:
+#            arg2(type): both of public and private 
+#            arg3(exclude_archive): true
+#        """
+#        conv_list (argv[1:])
+#
 #    elif command == "post_quote":
 #        post (str(argv[1]))
 #    elif command == "notice":
