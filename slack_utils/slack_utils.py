b6c90bd4b9e9e23c55dc48c17093268fc8449a68
mollinaca <github@mail.watarinohibi.tokyo>
2020-01-05 17:14:34 +0900

    update usage() の出力の仕方を調整、文面はあとでいれる

---
+++ slack_utils/slack_utils.py
@@ -0,0 +1,72 @@
+import sys
+import logging
+from optparse import OptionParser
+from slack_utils.functions import api_test, auth_test, incoming_webhook, post, conv_list
+"""
+実行例:
+  $ slack_utils api_test
+  $ slack_utils incoming_webhook "${message}"
+  $ slack_utils post "${message}"
+  $ slack_utils conv_list
+  $ slack_utils conv_list public
+  $ slack_utils conv_list private
+"""
+
+def usage ():
+    """
+    output usage
+    """
+    message = ("Usage: Todo")
+    print (message)
+    exit (1)
+
+def command (argv:list):
+    global command
+    command = argv[0]
+
+    if command == "test":
+        print ("test")
+        exit (0)
+    elif command == "api_test":
+        api_test ()
+
+    elif command == "auth_test":
+        auth_test ()
+
+    elif command == "incoming_webhook":
+        """
+        incoming-webhook を使ったシンプルなメッセージのPOST
+        """
+        if argv[1:2]:
+            incoming_webhook (argv[1:])
+        else:    
+            logging.warning('Usage -> $ slack_utils incoming_webhook ${message}')
+            exit (1)
+
+    elif command == "post":
+        if argv[1:2]:
+            post (argv[1:])
+        else:    
+            logging.warning('Usage -> $ slack_utils post ${message}')
+            exit (1)
+
+    elif command == "conv_list":
+        """
+        $ slack_utils conv_list [public|private] [true|false]'
+        default:
+            arg2(type): both of public and private 
+            arg3(exclude_archive): true
+        """
+        conv_list (argv[1:])
+
+#    elif command == "post_quote":
+#        post (str(argv[1]))
+#    elif command == "notice":
+#        notice (str(argv[1]))
+#    elif command == "warning":
+#        warning (str(argv[1]))
+#    elif command == "alert":
+#        alert (str(argv[1]))
+
+    else:
+        usage ()
